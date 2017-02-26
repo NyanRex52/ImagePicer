eec2f5e636953ae8f9e00d6f2d0e5b48e73580eb
Mohammed Al-Safwan <Nyanrex52@gmail.com>
2017-02-26 00:59:04 -0400

    Create ImagePicker.java

---
+++ ImagePicker.java
@@ -0,0 +1,186 @@
+public class ImagePicker {
+    private static final int DEFAULT_MIN_WIDTH_QUALITY = 400;        // min pixels
+    private static final String KEY_CLASS_NAME = "ImagePicker";
+    private static final String TEMP_IMAGE_NAME = "TempImage";
+    private static File imageFile;
+    public static int minWidthQuality = DEFAULT_MIN_WIDTH_QUALITY;
+    
+     /**
+     * All pictures will come in the data pack, even for the camera.
+     * instead of making the camera save to file, we can treat it as
+     * coming from a gallery.
+     *
+     * @param context
+     **/
+    public static Intent getPickImageIntent(Context context) {
+        imageFile = new File(context.getFilesDir(), TEMP_IMAGE_NAME);
+        Intent chooserIntent = null;
+
+        List<Intent> intentList = new ArrayList<>();
+
+        Intent pickIntent = new Intent(Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        takePhotoIntent.putExtra("return-data", true);
+        intentList = addIntentsToList(context, intentList, pickIntent);
+        intentList = addIntentsToList(context, intentList, takePhotoIntent);
+
+        if (intentList.size() > 0) {
+            chooserIntent = Intent.createChooser(intentList.remove(intentList.size() - 1),
+                    context.getResources().getString(R.string.pick_image_intent));
+            chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, intentList.toArray(new Parcelable[]{}));
+        }
+
+        return chooserIntent;
+    }
+    
+    private static List<Intent> addIntentsToList(Context context, List<Intent> list, Intent intent) {
+        List<ResolveInfo> resInfo = context.getPackageManager().queryIntentActivities(intent, 0);
+        for (ResolveInfo resolveInfo : resInfo) {
+            String packageName = resolveInfo.activityInfo.packageName;
+            Intent targetedIntent = new Intent(intent);
+            targetedIntent.setPackage(packageName);
+            if (!packageName.equals("com.google.android.GoogleCamera")) {
+                list.add(targetedIntent);
+                Log.d(KEY_CLASS_NAME, "Intent: " + intent.getAction() + " package: " + packageName);
+            }
+        }
+        return list;
+    }
+
+    /**
+     * Get Bitmap for the image from the given Intent.
+     * Use inside onActivityResult.
+     *
+     * @param context
+     * @param resultCode
+     * @param imageReturnedIntent
+     **/
+    public static Bitmap getBitmapFromResult(Context context, int resultCode,
+                                             Intent imageReturnedIntent) {
+        Log.d(KEY_CLASS_NAME, "getBitmapFromResult, resultCode: " + resultCode);
+        Bitmap bm = null;
+        Uri selectedImage = null;
+        if (resultCode == Activity.RESULT_OK) {
+            boolean isCamera = (imageReturnedIntent == null ||
+                    imageReturnedIntent.getData() == null ||
+                    imageReturnedIntent.getData().toString().contains(imageFile.toString()));
+            try {
+                selectedImage = imageReturnedIntent.getData();
+            } catch (NullPointerException e) {
+                e.printStackTrace();
+            }
+
+            Log.d(KEY_CLASS_NAME, "selectedImage: " + selectedImage);
+
+            // convert to bitmap and resize
+            bm = getImageResized(context, selectedImage);
+            int rotation = getRotation(context, selectedImage, isCamera);
+            bm = rotate(bm, rotation);
+        }
+        return bm;
+    }
+    
+    private static Bitmap decodeBitmap(Context context, Uri theUri, int sampleSize) {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inSampleSize = sampleSize;
+
+        AssetFileDescriptor fileDescriptor = null;
+        try {
+            fileDescriptor = context.getContentResolver().openAssetFileDescriptor(theUri, "r");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        assert fileDescriptor != null;
+        Bitmap actuallyUsableBitmap = BitmapFactory.decodeFileDescriptor(
+                fileDescriptor.getFileDescriptor(), null, options);
+
+        Log.d(KEY_CLASS_NAME, options.inSampleSize + " sample method bitmap ... " +
+                actuallyUsableBitmap.getWidth() + "*" + actuallyUsableBitmap.getHeight());
+
+        return actuallyUsableBitmap;
+    }
+
+    /**
+     * Resize to avoid using too much memory loading big images (e.g.: 2560*1920)
+     **/
+    private static Bitmap getImageResized(Context context, Uri selectedImage) {
+        Bitmap bm = null;
+        int[] sampleSizes = new int[]{5, 3, 2, 1};
+        int i = 0;
+        do {
+            bm = decodeBitmap(context, selectedImage, sampleSizes[i]);
+            i++;
+        } while (bm.getWidth() < minWidthQuality && i < sampleSizes.length);
+        return bm;
+    }
+
+    private static int getRotation(Context context, Uri imageUri, boolean isCamera) {
+        int rotation;
+        if (isCamera) {
+            rotation = getRotationFromCamera(context, imageUri);
+        } else {
+            rotation = getRotationFromGallery(context, imageUri);
+        }
+        Log.d(KEY_CLASS_NAME, "Image rotation: " + rotation);
+        return rotation;
+    }
+
+    private static int getRotationFromCamera(Context context, Uri imageFile) {
+        int rotate = 0;
+        try {
+
+            context.getContentResolver().notifyChange(imageFile, null);
+            ExifInterface exif = new ExifInterface(imageFile.getPath());
+            int orientation = exif.getAttributeInt(
+                    ExifInterface.TAG_ORIENTATION,
+                    ExifInterface.ORIENTATION_NORMAL);
+
+            switch (orientation) {
+                case ExifInterface.ORIENTATION_ROTATE_270:
+                    rotate = 270;
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_180:
+                    rotate = 180;
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_90:
+                    rotate = 90;
+                    break;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return rotate;
+    }
+
+    public static int getRotationFromGallery(Context context, Uri imageUri) {
+        int result = 0;
+        String[] columns = {MediaStore.Images.Media.ORIENTATION};
+        Cursor cursor = null;
+        try {
+            cursor = context.getContentResolver().query(imageUri, columns, null, null, null);
+            if (cursor != null && cursor.moveToFirst()) {
+                int orientationColumnIndex = cursor.getColumnIndex(columns[0]);
+                result = cursor.getInt(orientationColumnIndex);
+            }
+        } catch (Exception e) {
+            //Do nothing
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }//End of try-catch block
+        return result;
+    }
+
+    public static Bitmap rotate(Bitmap bm, int rotation) {
+        if (rotation != 0) {
+            Matrix matrix = new Matrix();
+            matrix.postRotate(rotation);
+            return Bitmap.createBitmap(bm, 0, 0, bm.getWidth(), bm.getHeight(), matrix, true);
+        }
+        return bm;
+    }
+
+}
